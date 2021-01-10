5f8a45ef5554267ca7c54e009ceb22482e640fb3
Jacopo Martinelli <jacopo.patroclo.martinelli@gmail.com>
2021-01-10 16:34:41 +0100

    fix(global): run migrations and del mig.json

--- packages/react-native-expo/src/utils/ensure-node-modules-symlink.ts
+++ packages/react-native-expo/src/utils/ensure-node-modules-symlink.ts
@@ -2,7 +2,7 @@ import { join } from 'path';
 import { existsSync, readFileSync, symlinkSync } from 'fs';
 import { createDirectory } from '@nrwl/workspace';
 import { platform } from 'os';
-import * as chalk from 'chalk'
+import * as chalk from 'chalk';
 
 export function ensureNodeModulesSymlink(
   workspaceRoot: string,
@@ -12,19 +12,27 @@ export function ensureNodeModulesSymlink(
   const packageJsonAppPath = join(projectRoot, 'package.json');
   const rootPackageJsonAppPath = join(workspaceRoot, 'package.json');
   const appNodeModulesPath = join(projectRoot, 'node_modules');
-  const rootDependency = getPackageJsonDependency(rootPackageJsonAppPath)
+  const rootDependency = getPackageJsonDependency(rootPackageJsonAppPath);
   if (!existsSync(appNodeModulesPath)) {
     createDirectory(appNodeModulesPath);
   }
-  for (const { name: moduleName, version: moduleVersion } of getProjectSymlynkDependency(packageJsonAppPath)) {
+  for (const {
+    name: moduleName,
+    version: moduleVersion,
+  } of getProjectSymlynkDependency(packageJsonAppPath)) {
     const modulePath = join(workspaceRoot, 'node_modules', moduleName);
     const moduleToPath = join(appNodeModulesPath, moduleName);
 
     // Handle scoped package
     if (isScopedPackage(moduleName)) {
-      const { scope, name } = getScopedData(moduleName)
+      const { scope, name } = getScopedData(moduleName);
       // Check version for expo cli
-      stopIfIsExpoScopedPackageWithVersionStar(scope, name, moduleVersion, rootDependency[moduleName])
+      stopIfIsExpoScopedPackageWithVersionStar(
+        scope,
+        name,
+        moduleVersion,
+        rootDependency[moduleName]
+      );
       const scopePath = join(appNodeModulesPath, scope);
 
       // If not exists create scoped directory
@@ -34,21 +42,28 @@ export function ensureNodeModulesSymlink(
     }
     // If the path not exists create symlink
     if (!existsSync(moduleToPath)) {
-      symlinkSync(modulePath, moduleToPath, symlinkType); 
+      symlinkSync(modulePath, moduleToPath, symlinkType);
     }
   }
 }
 
-function getPackageJsonDependency(packageJsonPath: string): { [name: string]: string } {
+function getPackageJsonDependency(
+  packageJsonPath: string
+): { [name: string]: string } {
   const packageJsonContent = JSON.parse(
     readFileSync(packageJsonPath, { encoding: 'utf8' }).toString()
   );
-  return packageJsonContent.dependencies || {}
+  return packageJsonContent.dependencies || {};
 }
 
-function getProjectSymlynkDependency(packageJsonPath: string): Array<{ name: string, version: string }> {
-  const dependencies = getPackageJsonDependency(packageJsonPath)
-  return Object.keys(dependencies).map(depName => ({ name: depName, version: dependencies[depName] }));
+function getProjectSymlynkDependency(
+  packageJsonPath: string
+): Array<{ name: string; version: string }> {
+  const dependencies = getPackageJsonDependency(packageJsonPath);
+  return Object.keys(dependencies).map((depName) => ({
+    name: depName,
+    version: dependencies[depName],
+  }));
 }
 
 function isScopedPackage(moduleName: string) {
@@ -67,20 +82,27 @@ function getScopedData(moduleName: string) {
  * this is why this cli is handling this case, moreover it's hepling keeping consistance
  * between the version inside your root package.json and the application one, for all the "@expo" libraryes
  */
-function stopIfIsExpoScopedPackageWithVersionStar(scope: string, name: string, version: string, basePackageVersion: string) {
+function stopIfIsExpoScopedPackageWithVersionStar(
+  scope: string,
+  name: string,
+  version: string,
+  basePackageVersion: string
+) {
   if (scope.startsWith('@expo')) {
     if (version === '*') {
       throw new Error(
-        `Invalid version for @expo scoped package ${scope}/${name}. Change "${chalk.red(`${scope}/${name}`)}": "*" --> "${
-          chalk.blue(`${scope}/${name}": "${basePackageVersion}`)
-        }" Inside your application's package.json`
+        `Invalid version for @expo scoped package ${scope}/${name}. Change "${chalk.red(
+          `${scope}/${name}`
+        )}": "*" --> "${chalk.blue(
+          `${scope}/${name}": "${basePackageVersion}`
+        )}" Inside your application's package.json`
       );
-    } else if(version !== basePackageVersion) {
-      console.log(chalk.yellow(
-        `Inside the package.json of your application the package ${scope}/${name} use version ${
-          version
-        } should be using ${basePackageVersion} as in your base package.json`
-      ))
+    } else if (version !== basePackageVersion) {
+      console.log(
+        chalk.yellow(
+          `Inside the package.json of your application the package ${scope}/${name} use version ${version} should be using ${basePackageVersion} as in your base package.json`
+        )
+      );
     }
   }
 }
