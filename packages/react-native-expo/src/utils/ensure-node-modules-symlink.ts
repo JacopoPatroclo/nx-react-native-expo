f50b3c9422531c4a003361facd19a709093c45fb
Jacopo Martinelli <jacopo.patroclo.martinelli@gmail.com>
2021-01-10 15:51:33 +0100

    fix(react-native-expo): support scoped pk symlink

--- packages/react-native-expo/src/utils/ensure-node-modules-symlink.ts
+++ packages/react-native-expo/src/utils/ensure-node-modules-symlink.ts
@@ -1,98 +1,86 @@
-import * as chalk from 'chalk';
 import { join } from 'path';
+import { existsSync, readFileSync, symlinkSync } from 'fs';
+import { createDirectory } from '@nrwl/workspace';
 import { platform } from 'os';
-import * as fs from 'fs';
-import { createDirectory, readJsonFile } from '@nrwl/workspace';
-
-const packagesToSymlink = [
-  'react-native',
-  'jsc-android',
-  '@react-native-community/cli-platform-ios',
-  '@react-native-community/cli-platform-android',
-  'hermes-engine',
-  '@nrwl/react-native',
-  '@babel/runtime',
-];
+import * as chalk from 'chalk'
 
 export function ensureNodeModulesSymlink(
   workspaceRoot: string,
   projectRoot: string
 ) {
-  const appNodeModulesPath = join(projectRoot, 'node_modules');
-  // `mklink /D` requires admin privilege in Windows so we need to use junction
   const symlinkType = platform() === 'win32' ? 'junction' : 'dir';
-
-  createDirectory(appNodeModulesPath);
-
-  packagesToSymlink.forEach((p) => {
-    const dir = join(appNodeModulesPath, p);
-    if (!fs.existsSync(dir)) {
-      if (isScopedPackage(p))
-        createDirectory(join(appNodeModulesPath, getScopedData(p).scope));
-      fs.symlinkSync(locateNpmPackage(workspaceRoot, p), dir, symlinkType);
-    }
-    if (!fs.existsSync(join(dir, 'package.json'))) {
-      throw new Error(
-        `Invalid symlink ${chalk.bold(dir)}. Remove ${chalk.bold(
-          appNodeModulesPath
-        )} and try again.`
-      );
-    }
-  });
-}
-
-function locateNpmPackage(workspaceRoot: string, packageName: string): string {
-  const pnpmDir = join(workspaceRoot, 'node_modules/.pnpm');
-  const isPnpm = fs.existsSync(pnpmDir);
-  if (!isPnpm) {
-    return join(workspaceRoot, 'node_modules', packageName);
+  const packageJsonAppPath = join(projectRoot, 'package.json');
+  const rootPackageJsonAppPath = join(workspaceRoot, 'package.json');
+  const appNodeModulesPath = join(projectRoot, 'node_modules');
+  const rootDependency = getPackageJsonDependency(rootPackageJsonAppPath)
+  if (!existsSync(appNodeModulesPath)) {
+    createDirectory(appNodeModulesPath);
   }
+  for (const { name: moduleName, version: moduleVersion } of getProjectSymlynkDependency(packageJsonAppPath)) {
+    const modulePath = join(workspaceRoot, 'node_modules', moduleName);
+    const moduleToPath = join(appNodeModulesPath, moduleName);
 
-  let candidates: string[];
-  if (isScopedPackage(packageName)) {
-    const { scope, name } = getScopedData(packageName);
-    const scopedDir = join(pnpmDir, scope);
-    candidates = fs
-      .readdirSync(scopedDir)
-      .filter((f) => f.startsWith(name))
-      .map((p) => join(scope, p));
-  } else {
-    candidates = fs
-      .readdirSync(pnpmDir)
-      .filter(
-        (f) =>
-          f.startsWith(packageName) &&
-          fs.lstatSync(join(pnpmDir, f)).isDirectory()
-      );
-  }
+    // Handle scoped package
+    if (isScopedPackage(moduleName)) {
+      const { scope, name } = getScopedData(moduleName)
+      // Check version for expo cli
+      stopIfIsExpoScopedPackageWithVersionStar(scope, name, moduleVersion, rootDependency[moduleName])
+      const scopePath = join(appNodeModulesPath, scope);
 
-  if (candidates.length === 0) {
-    throw new Error(`Could not locate pnpm directory for ${packageName}`);
-  } else if (candidates.length === 1) {
-    return join(pnpmDir, candidates[0], 'node_modules', packageName);
-  } else {
-    const packageJson = readJsonFile(join(workspaceRoot, 'package.json'));
-    const deps = {
-      ...packageJson.dependencies,
-      ...packageJson.devDependencies,
-    };
-    const version = deps['react-native'];
-    const found = candidates.find((c) => c.includes(version));
-    if (found) {
-      return join(pnpmDir, found, 'node_modules', packageName);
-    } else {
-      throw new Error(
-        `Cannot find ${packageName}@${version}. Install it with 'pnpm install --save ${packageName}@${version}'.`
-      );
+      // If not exists create scoped directory
+      if (!existsSync(scopePath)) {
+        createDirectory(scopePath);
+      }
+    }
+    // If the path not exists create symlink
+    if (!existsSync(moduleToPath)) {
+      symlinkSync(modulePath, moduleToPath, symlinkType); 
     }
   }
 }
 
-function isScopedPackage(p) {
-  return p.startsWith('@');
+function getPackageJsonDependency(packageJsonPath: string): { [name: string]: string } {
+  const packageJsonContent = JSON.parse(
+    readFileSync(packageJsonPath, { encoding: 'utf8' }).toString()
+  );
+  return packageJsonContent.dependencies || {}
 }
 
-function getScopedData(p) {
-  const [scope, name] = p.split('/');
+function getProjectSymlynkDependency(packageJsonPath: string): Array<{ name: string, version: string }> {
+  const dependencies = getPackageJsonDependency(packageJsonPath)
+  return Object.keys(dependencies).map(depName => ({ name: depName, version: dependencies[depName] }));
+}
+
+function isScopedPackage(moduleName: string) {
+  return moduleName.startsWith('@');
+}
+
+function getScopedData(moduleName: string) {
+  const [scope, name] = moduleName.split('/');
   return { scope, name };
+}
+
+/**
+ * A word here is needed
+ * This function is here becase of expo cli and how it handles dependencies
+ * In short, you can't use the wildcard character '*' as version number
+ * this is why this cli is handling this case, moreover it's hepling keeping consistance
+ * between the version inside your root package.json and the application one, for all the "@expo" libraryes
+ */
+function stopIfIsExpoScopedPackageWithVersionStar(scope: string, name: string, version: string, basePackageVersion: string) {
+  if (scope.startsWith('@expo')) {
+    if (version === '*') {
+      throw new Error(
+        `Invalid version for @expo scoped package ${scope}/${name}. Change "${chalk.red(`${scope}/${name}`)}": "*" --> "${
+          chalk.blue(`${scope}/${name}": "${basePackageVersion}`)
+        }" Inside your application's package.json`
+      );
+    } else if(version !== basePackageVersion) {
+      console.log(chalk.yellow(
+        `Inside the package.json of your application the package ${scope}/${name} use version ${
+          version
+        } should be using ${basePackageVersion} as in your base package.json`
+      ))
+    }
+  }
 }
