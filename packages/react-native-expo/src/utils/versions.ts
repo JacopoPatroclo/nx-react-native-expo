bb339721f56bc97c45ef9234a36533d0c4788190
Baptiste Marcon <baptistemcn@icloud.com>
2021-10-30 11:36:18 +0200

    chore: bump version

--- packages/react-native-expo/src/utils/versions.ts
+++ packages/react-native-expo/src/utils/versions.ts
@@ -1,21 +1,20 @@
 export const nxVersion = '*';
 
-export const reactVersion = '16.13.1';
-export const typesReactVersion = '16.9.35';
-export const reactNativeVersion =
-  'https://github.com/expo/react-native/archive/sdk-39.0.4.tar.gz';
-export const typesReactNativeVersion = '0.63.2';
-export const expoVersion = '39.0.2';
-export const expoStatusBarVersion = '1.0.2';
-export const reactDomVersion = '16.13.1';
-export const reactNativeWebVersion = '0.13.12';
-export const typesReactDomVersion = '16.9.8';
+export const reactVersion = '17.0.1';
+export const typesReactVersion = '17.0.21';
+export const reactNativeVersion = '0.64.2';
+export const typesReactNativeVersion = '0.64.12';
+export const expoVersion = '43.0.0';
+export const expoStatusBarVersion = '1.1.0';
+export const reactDomVersion = '17.0.1';
+export const reactNativeWebVersion = '0.17.1';
+export const typesReactDomVersion = '17.0.10';
 // TODO JM: Update this to 4.0.17
-export const expoCliVersion = '3.28.5';
-export const testingLibraryReactNativeVersion = '7.1.0';
-export const testingLibraryJestNativeVersion = '3.4.3';
+export const expoCliVersion = '4.12.10';
+export const testingLibraryReactNativeVersion = '8.0.0';
+export const testingLibraryJestNativeVersion = '4.0.2';
 export const jestReactNativeVersion = '18.0.0';
-export const reactTestRendererVersion = '17.0.1';
-export const reactTestRendererTypesVersion = '16.9.3';
-export const tsconfigPathWebpackPluginVersion = '3.3.0';
-export const expoWebpackConfigVersion = '0.12.43';
+export const reactTestRendererVersion = '17.0.2';
+export const reactTestRendererTypesVersion = '17.0.1';
+export const tsconfigPathWebpackPluginVersion = '3.5.1';
+export const expoWebpackConfigVersion = '0.16.6';
